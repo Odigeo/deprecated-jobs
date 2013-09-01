75a3b7f6fd8ab3bc81f62ea6073558c2e2b5f98a
Peter Bengtson <peter@peterbengtson.com>
2013-09-01 15:23:25 +0200

    Workers init more general.

--- lib/async_job_daemons.rb
+++ lib/async_job_daemons.rb
@@ -13,23 +13,24 @@ N_WORKERS = 5
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
+ENV_PATH = File.expand_path('../../config/environment.rb', __FILE__)
 
 require 'daemons'
 
 options = {
-  dir_mode: :normal,
-  dir: RUN_TIME_DIR,
-  backtrace: true,
+  dir_mode:   :normal,
+  dir:        RUN_TIME_DIR,
+  backtrace:  true,
   log_output: false,
-  multiple: false,
-  monitor: true
+  multiple:   false,
+  monitor:    true
 }
 
 
 # The async job workers
 (0...N_WORKERS).each do |i| 
   Daemons.run_proc("async_job_worker_#{i}", options) do
-    require '/Users/pjotr/Projects/Ocean/jobs/config/environment.rb'
+    require ENV_PATH
     q = AsyncJobQueue.new basename: 'AsyncJobs'
     loop do
       q.poll { |qm| qm.process } rescue nil
@@ -37,12 +38,11 @@ options = {
   end
 end
 
-# A daemon to remove log entries older than a month (one per machine instance)
+# A daemon to purge jobs (one per machine instance)
 Daemons.run_proc("async_job_purger", options) do
-  require '/Users/pjotr/Projects/Ocean/jobs/config/application.rb'
-  Jobs::Application.initialize!
+  require ENV_PATH
   while true do
     # Do something here
-    sleep 86400 # Sleep for a day
+    sleep 1.hour
   end
 end
