8201b8b2eb9e84714e135572b56ff28d9c5da67d
Peter Bengtson <peter@peterbengtson.com>
2013-09-01 15:08:25 +0200

    Workers can now be started. See the README.

--- lib/async_job_daemons.rb
+++ lib/async_job_daemons.rb
@@ -2,22 +2,25 @@
 # The directory RUN_TIME_DIR must already exist, and this process
 # must have the right to create files in it.
 #
-
-require 'rubygems'
-require 'daemons'
-
-# The number of log workers to keep running at all times
-N_WORKERS = 5
-
-# Run time directory
 RUN_TIME_DIR = "/var/run/async_job_workers"
 
+#
+# The number of log workers to keep running at all times
+#
+N_WORKERS = 5
+
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+
+
+require 'daemons'
 
 options = {
   dir_mode: :normal,
   dir: RUN_TIME_DIR,
   backtrace: true,
-  log_output: true,
+  log_output: false,
   multiple: false,
   monitor: true
 }
@@ -26,6 +29,7 @@ options = {
 # The async job workers
 (0...N_WORKERS).each do |i| 
   Daemons.run_proc("async_job_worker_#{i}", options) do
+    require '/Users/pjotr/Projects/Ocean/jobs/config/environment.rb'
     q = AsyncJobQueue.new basename: 'AsyncJobs'
     loop do
       q.poll { |qm| qm.process } rescue nil
@@ -35,16 +39,10 @@ end
 
 # A daemon to remove log entries older than a month (one per machine instance)
 Daemons.run_proc("async_job_purger", options) do
+  require '/Users/pjotr/Projects/Ocean/jobs/config/application.rb'
+  Jobs::Application.initialize!
   while true do
     # Do something here
     sleep 86400 # Sleep for a day
   end
 end
-
-
-# j = 
-# AsyncJob.create steps: [{'url' => 'http://127.0.0.1', 'retry_exponent' => 3},
-#                         {'url' => 'http://127.0.0.2', 'retry_exponent' => 4}],
-#                 credentials: "bWFnbmV0bzp4YXZpZXI=", 
-#                 token: "hhhhhhhhhhhhhhh"
-# j.reload; pp j.attributes; nil
