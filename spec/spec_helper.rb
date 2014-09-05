274da251053024929da9feb59857926dbf81e96c
Peter Bengtson <peter@peterbengtson.com>
2014-09-05 11:21:23 +0200

    Spec helper corrected.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,7 +15,6 @@ require 'webmock/rspec'
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
-  config.raise_errors_for_deprecations!
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
